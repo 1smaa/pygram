0277295a0e029c7726e4f09934fba4ac6396e626
1smaa <77068062+1smaa@users.noreply.github.com>
2021-10-04 18:13:36 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from bot.spam import *
+from bot.interact import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name="pygram",
+      version="0.1",
+      description="Interact with content and users of Instagram",
+      author="Ismaele Lorenzon",
+      author_email="lorenzonismaele@gmail.com",
+      url="https://github.com/1smaa/pygram",
+      license="MIT License")
