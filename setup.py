eaa6aed413c548396ae3065e287622ceed6eefe9
1smaa <77068062+1smaa@users.noreply.github.com>
2021-10-04 18:34:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name="pygram",
-      version="0.3",
+      version="0.0.4",
       description="Interact with content and users of Instagram",
       author="Ismaele Lorenzon",
       author_email="lorenzonismaele@gmail.com",
