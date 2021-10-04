c643d2c25908bf8c51487e478ebbbc6d37d8baeb
1smaa <77068062+1smaa@users.noreply.github.com>
2021-10-04 19:01:27 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(name="pygram",
+      author="Ismaele Lorenzon",
+      packages=find_packages(),
+      version="0.0.7",
+      author_email="lorenzonismaele@gmail.com",
+      description="A Python package to interact with content and users on Instagram"
+      )
