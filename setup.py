a9c11a97643fa19d96f13f30e6ad5baea07afda7
1smaa <77068062+1smaa@users.noreply.github.com>
2021-10-04 19:01:03 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(name="pygram",
-      version="0.0.6",
-      description="Interact with content and users of Instagram",
-      author="Ismaele Lorenzon",
-      author_email="lorenzonismaele@gmail.com",
-      url="https://github.com/1smaa/pygram",
-      license="MIT License")
