5ecabba3545c44ac9714120af44ef92bbdb28918
1smaa <77068062+1smaa@users.noreply.github.com>
2021-10-05 16:10:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name="pynsta",
       author="Ismaele Lorenzon",
       packages=['pynsta'],
-      version="0.1.1",
+      version="0.1.2",
       author_email="lorenzonismaele@gmail.com",
       description="A Python package to interact with content and users on Instagram",
       license="MIT License",
